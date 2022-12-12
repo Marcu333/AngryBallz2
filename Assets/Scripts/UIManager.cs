b56740a8f60a14a4dfda861165eb8e7761cecbc5
Mihai <mickai55@yahoo.com>
2022-12-12 20:41:26 +0200

    map & torches

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,7 +19,7 @@ public class UIManager: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateTimer();
+        UpdateTimer(); 
     }
 
     private void UpdateTimer()
